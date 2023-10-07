6bf4749544c2aef25065fd2eee8667db977bf574
Shashwat <shashwattripathi1503@gmail.com>
2023-10-08 00:04:38 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
+
+
+const App = () => {
+    return (
+       <div ClassName="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+        
+              App
+       </div >
+    )
+}
+export default App
